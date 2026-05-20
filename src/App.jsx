15fd0a39628094b6bd4bf10602e687f748330c69
SyafiqSuki <syafiqahmadannaufal9@gmail.com>
2026-05-20 22:19:23 +0700

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import gsap from 'gsap'
 import Navbar from "./components/navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Cocktails from "./components/cocktails.jsx";
+import About from "./components/about.jsx";
 gsap.registerPlugin(ScrollTrigger);
 
 
@@ -12,6 +13,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
     
   )
